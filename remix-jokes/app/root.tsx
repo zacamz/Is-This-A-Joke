b9d65aaa8359114f309779ca7ec26dc9d22a4854
Zachary Johnson <vepped@gmail.com>
2023-04-20 19:53:20 -0400

    Implemented the CSS
    Was able to add CSS to the root of the webpage

--- remix-jokes/app/root.tsx
+++ remix-jokes/app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  Links,
   LiveReload, Outlet
 } from "@remix-run/react";
 
@@ -8,6 +9,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet/>
